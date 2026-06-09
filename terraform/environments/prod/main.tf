15e67be16ddd1ddd03ef245cd46365f9348ea3f7
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2026-06-09 14:05:11 +0200

    Configuring target email addresses for prod

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -3,7 +3,7 @@ module "notifications" {
 
   environment             = "prod"
   aws_region              = "eu-central-1"
-  notification_recipients = "Sebastian.Celejewski@wp.pl"
+  notification_recipients = "Sebastian.Celejewski@wp.pl,Ag.Celejewska@wp.pl,FilipCelejewski@wp.pl,MajaCelejewska@wp.pl"
   smtp_host               = "smtp.wp.pl"
   smtp_port               = 587
   smtp_from               = "Sebastian.Celejewski@wp.pl"
